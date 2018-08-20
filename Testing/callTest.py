f5a8fa3236103d448946bd63c463bad836d7caaa
juanrom02 <juanrom02@gmail.com>
2018-08-20 10:08:44 -0300

    Path agregados a callTest

--- Testing/callTest.py
+++ Testing/callTest.py
@@ -3,7 +3,16 @@ from mock import MagicMock, PropertyMock, patch, mock_open, call
 import mock
 import os
 import sys
-from package import sys.path
+
+os.chdir("..")
+	
+sys.path.append(os.getcwd())
+sys.path.append(os.path.abspath('Email/'))
+sys.path.append(os.path.abspath('Modem/'))
+sys.path.append(os.path.abspath('Network/'))
+sys.path.append(os.path.abspath('Bluetooth/'))
+sys.path.append(os.path.abspath('File Transfer/'))
+sys.path.append(os.path.abspath('Audio/'))
 
 import callClass
 import networkClass
